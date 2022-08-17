eab29d72387dd9c9d91d7722d30e01766ed02f1c
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-17 14:32:52 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Int32 y;
+            i = 10;
+            y = 20;
+            int r = i + y;
+            int p, j;
+            p=Convert.ToInt32(Console.ReadLine());
+            j=Convert.ToInt32(Console.ReadLine());
+            int k =p*j;
+            Console.WriteLine(k);
+            Console.WriteLine(r);
+            Console.WriteLine("Hello, World!");
+            Console.Write(DateTime.Now);
+        }
+    }
+}
